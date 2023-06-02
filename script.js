f67c69a438bf57cc892dcc19aafed66f6bef28ce
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-02 11:57:42 -0400

    Number Logic

--- script.js
+++ script.js
@@ -3,21 +3,15 @@
 
 
 
-// const COLORS = {
-//     null: 'blue',
-//     mine: 'gray',
-//     blank: 'yellow'
-// }
-
-
-
 const mine = `<img src='https://www.giantbomb.com/a/uploads/scale_medium/8/87790/3216800-icon_mine.png' height='100%'>`
 
 
 const PICTURES = {
     null: '',
     mine: mine,
-    number: `<p>3</p>`
+    1: `<p>1</p>`,
+    2: `<p>2</p>`,
+    3: `<p>3</p>`,
 }
 
 
@@ -34,10 +28,10 @@ init()
 function init(){
 
 board = [
-    [null, 'number', null, null, null],
-    [null, 'mine', null, null, null],
     [null, null, null, null, null],
-    [null, null, null, 'mine', null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
     [null, null, null, null, null],
 ]
 
@@ -56,7 +50,8 @@ function render(){
 
 
 function renderBoard(){
-    
+    handleBombLocations()
+
     for(let i = 0; i <= board.flat().length - 1; i++){
       let val = document.querySelector(`#board :nth-child(${i + 1})`)
 
@@ -72,7 +67,7 @@ function renderBoard(){
 
 function getBombLocations(){
 bombLocations = []
-    while(bombLocations.length < 10){
+    while(bombLocations.length < 5){
         let rando = Math.floor(Math.random() * 25)
     if(!bombLocations.includes(rando)){
         bombLocations.push(rando)
@@ -81,4 +76,48 @@ bombLocations = []
     console.log(bombLocations.sort((a,b) => a - b), 'bombLocations')
 }
 
+
+
+function handleBombLocations(){
 getBombLocations()
+
+let newBoard = board.flat()
+let final = [];
+let innerArr = [];
+
+// for(let i =0; i<= bombLocations.length; i++){
+//     newBoard[bombLocations[i]] = 'mine'
+// }
+
+bombLocations.forEach(e => newBoard[e] = 'mine')
+
+for(let i =0; i< newBoard.length; i++){
+if(newBoard[i] === 'mine'){
+    if(newBoard[i - 1] !== 'mine'){
+        if(newBoard[i - 1] === null){
+            newBoard[i - 1] = 1
+        } else {
+            newBoard[i - 1]++
+        }
+    }
+}
+}
+
+console.log(newBoard, 'newBoard')
+
+while(newBoard.length){
+    console.log('works')
+    innerArr.push(newBoard.shift())
+    console.log(innerArr)
+    if(innerArr.length === 5){
+        final.push(innerArr)
+        innerArr = []
+    }
+
+}
+
+console.log(final, 'final')
+board = final
+console.log(board, 'board')
+
+}
